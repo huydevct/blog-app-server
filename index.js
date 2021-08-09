471aeaf20b7eb87d2f51366de387dcf1788716d7
huydev <64426508+huydevct@users.noreply.github.com>
2021-08-09 20:21:45 +0700

    uodate

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 const URI = process.env.DATABASE_URL;
 
-
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
